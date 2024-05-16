58f7f46fc900e502a13411576a9231bd500f8908
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-16 16:25:04 +0200

    FINISH SNACK 1

--- snack1/main.js
+++ snack1/main.js
@@ -3,7 +3,7 @@
 const bicycles = [
     {
         nome: 'bicicletta da corsa',//NOME DELL'OGGETTO
-        peso: 10,//PESO DELL'OGGETTO
+        peso: 20,//PESO DELL'OGGETTO
     },
     {
         nome: 'Mountain bike',
@@ -23,20 +23,31 @@ const bicycles = [
     }
     
 ];
+const bicyclesWeigh =[];//CREO UN ARRAY VUOTO DOVE VADO A PUSHARE I PESI DELLE BICICLETTE
+
+bicycles.forEach(element=> bicyclesWeigh.push(element.peso))//PUSHO I PESI DELLE BICICLETTE NELL'ARRAY
+
+const bicyclesWeighlight= (arrayMin(bicyclesWeigh));//TROVO IL NUMERO PIU PICCOLO TRA I NUMERI CHE CI SONO NELL'ARRAY BICYCLESWEIGH
+
+const bicycleslightindex = bicyclesWeigh.indexOf(bicyclesWeighlight);//TROVO L'INDICE DELL'NUMERO PIU PICCOLO
+
+const bicycleslight = bicycles[bicycleslightindex].nome;//RICAVO IN BASE ALL'INDICE DELL'NUMERO PIU PICCOLO IL NOME DELLA BICICLETTA
 
 
-const bicyclesWeigh =[];
-
-console.log(bicycles)
-
-bicycles.forEach(element=> bicyclesWeigh.push(element.peso))
 
 
 
 
 
+console.log (`La bicicletta piu leggera e la ${bicycleslight} con un peso di ${bicyclesWeighlight} `)//SCRIVO IN CONSOLE QUALE è LA BICICLETTA PIU LEGGERA
 
 
-function arrayMin(arr) {
+
+function arrayMin(arr) {//FUNZIONE PER TROVARE IL NUMERO PIU PICCOLO ALL'INTERNO DI UN ARRAY
     return Math.min.apply(null, arr);
   }
+
+
+
+
+
